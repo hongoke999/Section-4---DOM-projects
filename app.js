13d908ecdcefc8f09258ee0068dce1eef54e8050
Chaka Hongoke <hongoke999@yahoo.com>
2020-05-08 11:07:31 +0300

    Task List [Part 2] - Delete & Filter Tasks

--- app.js
+++ app.js
@@ -12,7 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
     //  Add task event
     form.addEventListener('submit', addTask);
-    
+    //  Remove task event
+    taskList.addEventListener('click', removeTask);
+    //  Clear task event
+    clearBtn.addEventListener('click', clearTasks);    
+    //  Filter tasks events
+    filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -44,4 +49,39 @@ function addTask(e) {
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+//  Remove Task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        if(confirm('Are You Sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }        
+    }    
+}
+
+//  Clear Tasks
+function clearTasks(e) {
+    // taskList,innerHTML = '';
+
+    // Faster
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+
+    // https:/jsperf.com/innerhtml-vs-removeChild
+}
+
+//  Filter Tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    }); 
 }
